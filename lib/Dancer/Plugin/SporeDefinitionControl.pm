735912e65c2078457641b65e78c8f5947fe654c7
Nicolas Oudard <noudard+theboss@weborama.com>
2016-02-24 10:41:24 +0100

    Bump to version 0.14

--- lib/Dancer/Plugin/SporeDefinitionControl.pm
+++ lib/Dancer/Plugin/SporeDefinitionControl.pm
@@ -17,11 +17,11 @@ Dancer Plugin to control validity of route from a Spore configuration file
 
 =head1 VERSION
 
-Version 0.13
+Version 0.14
 
 =cut
 
-our $VERSION = '0.13';
+our $VERSION = '0.14';
 
 =head1 SYNOPSIS
 
