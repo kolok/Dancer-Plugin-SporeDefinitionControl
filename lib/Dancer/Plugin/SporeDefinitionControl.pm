f54550c3565c47dc2b5275a8d05d81bebf071a23
Frederic Lechauve <frederic@weborama.com>
2019-01-25 16:22:31 +0100

    add the capability to manage OPTIONS

--- lib/Dancer/Plugin/SporeDefinitionControl.pm
+++ lib/Dancer/Plugin/SporeDefinitionControl.pm
@@ -94,12 +94,14 @@ Load yaml config file
 #Load definition spore file from plugin config
 
 our $path_validation;
-
+#_load_path_validation();
+$path_validation =_load_path_validation();
 sub _load_path_validation
 {
     my $rh_file = {};
 
     my $path_to_spore_def = plugin_setting->{'spore_spec_path'};
+    my $spore_spec_with_options = plugin_setting->{'spore_spec_with_options'};
     if ($path_to_spore_def)
     {
     $path_to_spore_def = File::Spec->catfile( setting('appdir') , $path_to_spore_def) unless (File::Spec->file_name_is_absolute($path_to_spore_def));
@@ -107,11 +109,10 @@ sub _load_path_validation
     }
 
     my $path_valid;
+
     #load validation hash
     foreach my $method_name (keys(%{$rh_file->{'methods'}}))
     {
-
-
         my $method = $rh_file->{'methods'}->{$method_name}->{'method'};
         my $complet_path = $rh_file->{'methods'}->{$method_name}->{'path'};
         my ($path, $query_params) = split(/\?/, $complet_path);
@@ -122,17 +123,43 @@ sub _load_path_validation
             push @{$rh_file->{'methods'}->{$method_name}->{'required_params'}}, @additional_params;
         }
 
-
-        push @{$path_valid->{$method}->{$path}->{params}},
+        push @{$path_valid->{path}->{$path}}, $method;
+        push @{$path_valid->{method}->{$method}->{$path}->{params}},
           {
             required_params => $rh_file->{'methods'}->{$method_name}->{'required_params'},
             optional_params => $rh_file->{'methods'}->{$method_name}->{'optional_params'},
             form_data_params => $rh_file->{'methods'}->{$method_name}->{'form-data'},
           };
-        $path_valid->{$method}->{$path}->{functions}->{$method_name} = 1;
+        $path_valid->{method}->{$method}->{$path}->{functions}->{$method_name} = 1;
+
     }
+    init_routes_options($path_valid->{path}) if (defined $spore_spec_with_options);
     return $path_valid;
 };
+
+=head2 init_routes_options
+
+force the routes on options method
+
+=cut
+
+
+
+sub init_routes_options(){
+    my $paths = shift;
+    foreach my $path (keys %{$paths}){
+            options $path => sub {
+#                status 200;
+#                header 'access-control-allow-credentials' => plugin_setting->{'spore_spec_allow_credentials'} || '';
+#                header 'access-control-allow-headers' => plugin_setting->{'spore_spec_allow_headers'} || '';
+#                header 'access-control-allow-methods' => join (',',$paths->{$path}).",'OPTIONS'";
+#                header 'access-control-allow-origin' => plugin_setting->{'spore_spec_'} || '';
+#                header 'access-control-max-age' => plugin_setting->{'spore_spec_'}  || '';
+#                return halt('{"status":200,"message":"OK"}');
+            };
+
+        }
+}
 
 
 
@@ -151,25 +178,30 @@ register 'check_spore_definition' => sub {
         my %req_params = params;
         die "method request must be defined" unless (defined( $req->method() ) );
         _returned_error( "route pattern request must be defined", 404) unless (defined( $req->{_route_pattern} ) );
+
 #        my $all_route_pattern = $req->{_route_pattern};
 #my $detail_route_pattern =  split /?/, $route_pattern;
-        $path_validation = _load_path_validation() if !$path_validation;
-        unless (defined( $path_validation->{$req->method()} ) )
+        #$path_validation = _load_path_validation() if !$path_validation;
+        unless (defined( $path_validation->{method}->{$req->method()}) || uc($req->method()) eq "OPTIONS" )
         {
           my $req_method = $req->method();
           return _returned_error("no route define with method `$req_method'", 404);
         }
-#TODO : return an error because path does not exist in specification
-        unless (defined( $path_validation->{$req->method()}->{$req->{_route_pattern}} ) )
+
+        #TODO : return an error because path does not exist in specification
+        unless (defined( $path_validation->{method}->{$req->method()}->{$req->{_route_pattern}} ) 
+                        || (uc($req->method()) eq "OPTIONS" && defined($path_validation->{path}->{$req->{_route_pattern}}))
+                )
         {
           my $req_route_pattern = $req->{_route_pattern};
           return _returned_error("route pattern `$req_route_pattern' is not defined",404);
         }
 
-
         my $is_ok = 0;
+        #IF method is OPTIONS list of methods set the headers and return ok
+        _returned_options_methods($path_validation->{path}->{$req->{_route_pattern}}) if (uc($req->method()) eq "OPTIONS" );
         my $error;
-        foreach my $route_defined (@{$path_validation->{$req->method()}->{$req->{_route_pattern}}->{params}})
+        foreach my $route_defined (@{$path_validation->{method}->{$req->method()}->{$req->{_route_pattern}}->{params}})
         {
             my $ko;
             my $ra_required_params = $route_defined->{'required_params'};
@@ -228,7 +260,7 @@ register 'get_functions_from_request' => sub {
     $path_validation = _load_path_validation() if !$path_validation;
     my $method = $req->method();
     my $path = $req->{_route_pattern};
-    my $functions = $path_validation->{$method}->{$path}->{functions};
+    my $functions = $path_validation->{method}->{$method}->{$path}->{functions};
     return $functions;
 };
 
@@ -252,6 +284,29 @@ sub _returned_error
   else
   {die "Unknown code";}
 }
+
+sub _returned_options_methods
+{
+  my $methods = shift;
+  my %seen = ();
+  my @unique_methods = grep { !$seen{$_}++ } @{$methods};
+  if (defined $methods){
+  set serializer => 'JSON';
+  status 200;
+  header 'access-control-allow-credentials' => plugin_setting->{'spore_spec_allow_credentials'} || '';
+  header 'access-control-allow-headers' => plugin_setting->{'spore_spec_allow_headers'} || '';
+  header 'access-control-allow-methods' => join(",",@unique_methods).",OPTIONS";
+  header 'access-control-allow-origin' => plugin_setting->{'spore_spec_allow_origin'} || '';
+  header 'access-control-max-age' => plugin_setting->{'spore_spec_max_age'}  || '';
+  return halt('{"status":200,"message":"OK"}');
+  }
+  else{
+      set serializer => 'JSON';
+      status 404;
+      return halt('{"status":404,"message":"no route exists"}');
+  }
+}
+
 
 =head1 AUTHOR
 
